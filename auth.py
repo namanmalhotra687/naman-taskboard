ce97128f23a7d7f8a7cf079412fb820ac83d0094
Naman Malhotraa <Naman.9.Malhotraa@in.niit.com>
2025-07-14 13:00:59 +0530

    Fix authenticate_user argument error

--- auth.py
+++ auth.py
@@ -1,16 +1,14 @@
 from sqlalchemy.orm import Session
-from fastapi import Request
-
 from model import User
 
 def authenticate_user(db: Session, username: str, password: str):
-    """Authenticate user with database"""
-    return db.query(User).filter(User.username == username, User.password == password).first()
+    user = db.query(User).filter(User.username == username).first()
+    if user and user.password == password:
+        return user
+    return None
 
-def login_user(request: Request, user: User):
-    """Login the user by setting session"""
-    request.session['user'] = user.username
+def login_user(request, user):
+    request.session["user"] = user.username
 
-def logout_user(request: Request):
-    """Logout user by clearing session"""
-    request.session.pop('user', None)
+def logout_user(request):
+    request.session.clear()
